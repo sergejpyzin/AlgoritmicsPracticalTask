aef6bc6a893f4c45d929a657b1f77170b0056916
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-06-24 15:07:18 +0300

    deleted BinaryTree

---
+++ src/main/java/LessonsFour/Main.java
@@ -0,0 +1,22 @@
+package LessonsFour;
+
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        binaryTree.add(6);
+        binaryTree.add(3);
+        binaryTree.add(2);
+        binaryTree.add(4);
+        binaryTree.add(7);
+        binaryTree.add(8);
+        binaryTree.add(9);
+
+
+
+        binaryTree.print();
+
+    }
+
+
+
+}
